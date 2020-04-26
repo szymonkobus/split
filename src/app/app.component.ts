be5687d7a4f0cd4b81616f42a15e3f6181174791
szymonkobus <36537361+szymonkobus@users.noreply.github.com>
2020-04-26 19:16:40 +0200

    initial commit - app prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,57 @@
 import { Component } from '@angular/core';
+//import { MatListOption } from '@angular/material/list'; 
+
+import { ActivatedRoute } from "@angular/router";
+import { isDefined } from '@angular/compiler/src/util';
+//import { Router } from "@angular/router";
+//import { Router, RoutesRecognized } from '@angular/router';
+
+interface transaction {
+  balance: number;
+  change: number;
+}
+
+class Wager {
+  transaction_history: {[name: string]: transaction}[]
+  columns: string[]
+  
+  constructor() {
+    this.columns = []
+    this.transaction_history = [{}]
+  }
+  
+  addTransaction(cost:number, payer_obj: any [], payees_obj: any []) {
+    // validate input
+    if (cost <=0 || isNaN(cost) || payer_obj.length==0 || payees_obj.length==0 ) { return }
+    
+    const payees: string [] = payees_obj.map( e => e._value)
+    const payer: string = payer_obj[0]._value
+    const part = cost / payees_obj.length
+    const last_state = this.transaction_history[0]
+
+    var new_state = {}
+    for (var key in last_state) {
+      new_state[key] = {balance: last_state[key].balance, change: 0}
+    }
+
+    for (var payee of payees) {
+      new_state[payee].balance = new_state[payee].balance - part
+      new_state[payee].change  = -1 * part
+    }
+    new_state[payer].balance += cost
+    new_state[payer].change  += cost
+
+    this.transaction_history.unshift(new_state)
+  }
+
+  addPerson( name:string ){
+    if (name=="" || this.columns.includes(name)) { return } // validate input
+    this.columns.push(name);
+    this.transaction_history.map( (dic) => {dic[name]={balance:0,change:0}; return dic})
+  }
+}
+
+const wager:Wager = new Wager()
 
 @Component({
   selector: 'app-root',
@@ -6,5 +59,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'split';
+  names = wager.columns
+  transaction_history = wager.transaction_history
+
+  //url data
+  initial_names: string
+  // form input
+  payer: string
+  payees: string []
+
+  constructor( private route: ActivatedRoute){
+    this.route.queryParams.subscribe(params => {
+      if(params.ppl == undefined) { return }
+      for (let name of params.ppl.split(";") )
+      { wager.addPerson(name) }
+    })
+  }
+
+  ngOnInit(): void {
+    console.log(this.initial_names)
+  }
+  addRow(cost:number, payer:any [], payees_obj: any [], HTMLtable){
+    console.log(payer)
+    wager.addTransaction(cost,payer,payees_obj)
+    HTMLtable.renderRows()  // re-render after adding row
+  }
+  addColumn(name:string){
+    wager.addPerson( name )
+  }
+  round(n:number){
+    return Math.round(n * 10) / 10
+  }
 }
+
+// TODO: reset transaction after clicking(only who pays maybe)
+// TODO: mamke everyone seleced at beginning (or selct all button)
+// TODO: change icaon at top
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//import {Component, ViewChild} from '@angular/core';
+
+import { MatTableModule } from '@angular/material/table'
+import { MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatListModule } from '@angular/material/list'; 
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatListModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    //ViewChild,
+    //Component,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
